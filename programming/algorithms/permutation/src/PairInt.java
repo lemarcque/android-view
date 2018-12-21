81a11087bc51b7d1345ca1556675022d6f05c476
lemarcque <hello@capsulo.io>
2018-12-21 18:00:35 +0100

    Added methods for permuting two integer. (don't work yet)

---
+++ programming/algorithms/permutation/src/PairInt.java
@@ -0,0 +1,76 @@
+/* *
+ * MIT License
+ *
+ * Copyright (c) 2018 Henoc Sese
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+public class PairInt extends Pair<Integer>{
+
+
+    public PairInt(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    PairInt permute(Integer x, Integer y) {
+        int tempX = x;
+
+        // Immutable object to ensure purity of the function
+        int _x = y;
+        int _y = tempX;
+
+
+        return new PairInt(_x, _y);
+    }
+
+    PairInt permuteXor(Integer x, Integer y) {
+        int _x;
+        int _y;
+
+        // XOR operand
+        _x = x ^ y;
+        _y = x ^ y;
+        _x = y ^ x;
+
+        return new PairInt(_x, _y);
+    }
+
+    PairInt permuteAdd(Integer x, Integer y) {
+        int _x = x;
+        int _y = y;
+
+        // Addition operand
+        _x = x + y;
+        _y = x - y;
+        _x = y + x;
+
+        return new PairInt(_x, _y);
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
