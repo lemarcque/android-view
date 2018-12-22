92273fb631564af9e3e74aca7248e37a8fed9aba
lemarcque <hello@capsulo.io>
2018-12-22 11:55:14 +0100

    added animation video

--- programming/algorithms/sorting/vizualizer/src/Main.java
+++ programming/algorithms/sorting/vizualizer/src/Main.java
@@ -1,31 +1,49 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Henoc Sese
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Main extends Application {
 
+    // Application propreties
     private static int APP_WIDTH = 400;
     private static int APP_HEIGHT = 400;
     private static String APP_NAME = "Sorting algorithm";
 
-    private static int MINIMUM_DATA = 10;
-    private static int MAXIMUM_DATA = 150;
+    // List
+    private List<Integer> barValueList = new ArrayList<>();
+    private List<Bar> barList;
 
-    final int maxWidth = 5;
-    int randBarGenerated;
-
-    List<Integer> unOrderList = new ArrayList<>();
-    List<Bar> barList;
-
+    // JavaFX node
     private Group root;
+    private Stage stage;
 
     public static void main(String[] args){
         launch(args);
@@ -37,34 +55,58 @@ public class Main extends Application {
 
         root = new Group();
         Scene scene = new Scene(root, APP_WIDTH, APP_HEIGHT);
-        primaryStage.setTitle(APP_NAME);
-        primaryStage.setScene(scene);
-        primaryStage.show();
 
+        stage = primaryStage;
+        stage.setTitle(APP_NAME);
+        stage.setScene(scene);
+        stage.show();
 
-        randBarGenerated = 20; // int randValue = min + (int) (Math.random() * (max - min);
-
-        // generate set of datas
-        generateDisorderList();
+        // generate set of data
         generateList();
-        disorderList();
+        displayList();
+
+        // List to keyboard event
+        keyboardEventManager();
+
+        // Algorithms of sorting
+        // TODO : Bubble sort
     }
 
+
+    /**
+     * Generate a list of integer representing
+     * value of the Bar
+     */
     private void generateList() {
+        // Generate a random integer of that define the number of data (i.e, Bar that will be displayed)
+        // final int min = 10;
+        // final int max = 150;
+        // int randValue = min + (int) (Math.random() * (max - min);
+
+        final int randValue = 20;
+
+        barValueList = new ArrayList<>();
+
+        // add item on the list
+        for(int i = 1; i < randValue + 1; i++) {
+            barValueList.add(i * (APP_HEIGHT / randValue));
+        }
+    }
+
+    /**
+     * Display bar chart with Bar object
+     */
+    private void displayList() {
         Bar a = new Bar(100, 100);
         Bar b = new Bar(100, 100);
 
 
-        System.out.println("a    :" + a);
-        System.out.println("b    :" + b);
-
-
         barList = new ArrayList<>();
-        final int width = (APP_WIDTH / randBarGenerated);   // width of bar
+        final int width = (APP_WIDTH / barValueList.size());   // width of bar
 
-        for (int i = 0; i < unOrderList.size(); i++) {
+        for (int i = 0; i < barValueList.size(); i++) {
 
-            int value = unOrderList.get(i);
+            int value = barValueList.get(i);
 
             // Add new rectangle of the root scene
             Bar bar = new Bar(value, width);
@@ -75,15 +117,9 @@ public class Main extends Application {
         }
     }
 
-    private void generateDisorderList() {
-        unOrderList = new ArrayList<>();
-
-        // add item on the list
-        for(int i = 1; i < randBarGenerated + 1; i++) {
-            unOrderList.add(i * (APP_HEIGHT / randBarGenerated));
-        }
-    }
-
+    /**
+     * Make the list in disorder
+     */
     private void disorderList() {
         int minValue = barList.get(0).getValue();
         int maxValue = barList.get(barList.size() -1 ).getValue();
@@ -91,20 +127,48 @@ public class Main extends Application {
 
         for(Bar bar : barList) {
 
-            int min = 0;
-            int max = barList.size();
-            int randElement = min + (int) (Math.random() * (max - min));
+            // Minimum and Maximum random element
+            final int min = 0;
+            final int max = barList.size();
+            final int randElement = min + (int) (Math.random() * (max - min));
 
-
+            // Get a random bar
             Bar randomBar = (Bar) root.getChildren().get(randElement);
             randomBar.setOrdered(false);
 
-
-            // swapping
+            // Swapping of x position (permutation)
             int tmpBarX = (int) bar.getLayoutX();
             bar.setLayoutX(randomBar.getLayoutX());
             randomBar.setLayoutX(tmpBarX);
+
+            // Not disorder, if equals to the random bar got
+            // TODO : Surely, this condition will never be satisfied
+            if(bar.getLayoutX() != randElement) {
+                bar.setOrdered(false);
+            }
         }
+    }
+
+    /**
+     * Handle keyboard press event
+     */
+    private void keyboardEventManager() {
+        stage.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+
+            switch (event.getCode()) {
+                
+                case SPACE:
+                    // generate new disorder list
+                    disorderList();
+                    break;
+                    
+                case ENTER:
+                    // order the list with an sorting algorithm's
+                    break;
+                    
+                    default:
+            }
+        });
     }
 
 }
